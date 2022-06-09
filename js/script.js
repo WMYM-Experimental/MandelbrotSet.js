b20bdbe03c4cf63c1db985921c1da63e2d054ad9
WashingtonYandun <washington.yandun06@gmail.com>
2022-06-08 22:33:46 -0500

    start making the mandelbrot function

--- js/script.js
+++ js/script.js
@@ -4,8 +4,19 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const init = () => {};
+const getRandomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.random() * (max - min) + min;
+};
 
-const animate = () => {
-    requestAnimationFrame(animate);
+const mandelbrot = (c, maxIterations) => {
+    let z = 0;
+    let n = 0;
+    while (n < maxIterations && z * z < 4) {
+        z = z * z + c;
+        console.log(z);
+        n++;
+    }
+    return n;
 };
