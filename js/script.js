52a5b3547f345e4f5e3c90b1f56e622a031ec04c
WashingtonYandun <washington.yandun06@gmail.com>
2022-06-17 23:38:32 -0500

    add a complex class for hanlding complex numbers

--- js/script.js
+++ js/script.js
@@ -4,6 +4,36 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+class Complex {
+    constructor(real, imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+    add(other) {
+        return new Complex(this.real + other.real, this.imag + other.imag);
+    }
+    sub(other) {
+        return new Complex(this.real - other.real, this.imag - other.imag);
+    }
+    mul(other) {
+        return new Complex(
+            this.real * other.real - this.imag * other.imag,
+            this.real * other.imag + this.imag * other.real
+        );
+    }
+    div(other) {
+        return new Complex(
+            (this.real * other.real + this.imag * other.imag) /
+                (other.real * other.real + other.imag * other.imag),
+            (this.imag * other.real - this.real * other.imag) /
+                (other.real * other.real + other.imag * other.imag)
+        );
+    }
+    mag() {
+        return Math.sqrt(this.real * this.real + this.imag * this.imag);
+    }
+}
+
 const getRandomNumber = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
